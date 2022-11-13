ed930fb1ddac3606461c18d3be28f25ef8f494fe
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-11-13 17:27:11 +0900

    mission3まで完了

--- crypto-dappy/src/hooks/use-collection.hook.js
+++ crypto-dappy/src/hooks/use-collection.hook.js
@@ -1,16 +1,60 @@
-import { query } from '@onflow/fcl'
+import { mutate, query, tx } from '@onflow/fcl'
 import { useEffect, useState } from 'react';
 import { CHECK_COLLECTION } from "../flow/check-collection.script";
+import { CREATE_COLLECTION  } from "./../flow/create-collection.tx";
+import { DELETE_COLLECTION  } from "./../flow/delete-collection.script";
 
 export default function useCollection(user) {
   const [loading, setLoading] = useState(true)
   const [collection, setCollection] = useState(false)
 
+  /**
+   * createCollection
+   */
   const createCollection = async () => {
-    setCollection(true)
+    try {
+      // mutate
+      let res = await mutate({
+        cadence: CREATE_COLLECTION,
+        limit: 55,
+      });
+
+      // tx 
+      await tx(res).onceSealed()
+
+      console.log("res:", res);
+      setLoading(false);
+      setCollection(true);
+    } catch(err) {
+      console.error("err:", err);
+      setLoading(false);
+    }
+
+    setCollection(true);
   }
 
+  /**
+   * deleteCollection
+   */
   const deleteCollection = async () => {
+    try {
+      // mutate
+      let res = await mutate({
+        cadence: DELETE_COLLECTION,
+        limit: 75,
+      });
+
+      // tx 
+      await tx(res).onceSealed()
+
+      console.log("res:", res);
+      setLoading(false);
+      setCollection(false);
+    } catch(err) {
+      console.error("err:", err);
+      setLoading(false);
+    }
+
     setCollection(false)
     window.location.reload()
   }
